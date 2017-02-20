1f3538f908e51ce66ef5dde2b7146abb0f2ec0b0
Bomin-Lee <pairagon0@gmail.com>
2017-02-20 19:21:17 +0900

    resources setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'comments/_form'
-
-  get 'comments/_comment'
-
-  get 'posts/new'
-
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'posts/index'
-
-  get 'posts/_form'
-
+  resources :posts do
+    resources :comments, only:[:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
