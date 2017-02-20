36d59a8621ce00e177cab635c715028f29a05c8b
Bomin-Lee <pairagon0@gmail.com>
2017-02-20 19:44:05 +0900

    Make create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
   end
 
   def show
@@ -14,4 +20,10 @@ class PostsController < ApplicationController
 
   def _form
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
 end
